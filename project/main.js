1ca4187b3b766e2b72c65341540e96752e1bead0
MILANA TRAN <MILANATRAN.NTM@gmail.com>
2022-05-05 20:41:44 +0200

    Made layout and partials work

--- project/main.js
+++ project/main.js
@@ -5,10 +5,15 @@ const layouts = require("express-ejs-layouts");
 const port = 3000,
 express = require("express"),
 app = express();
+
+//view engine setup
 app.set("view engine", "ejs");
+
+app.use(layouts);
 
 app.get("/chatroom/:url", homeController.sendReqParam);
 app.get("/name/:myName",homeController.respondWithName);
+
 
 app.use((req, res, next) => {
 console.log(`request made to: ${req.url}`);
@@ -21,15 +26,11 @@ app.use(
  })
 );
 app.use(express.json());
-app.use(layouts);
+
 
 //Get data being submitted
 app.post("/", homeController.showData);
-// app.post("/", (req, res) => {
-//  console.log(req.body);
-//  console.log(req.query);
-//  res.send("POST Successful!");
-// });
+
 
 app.listen(port, () => {
  console.log(`Server running on port: ${port}`);
