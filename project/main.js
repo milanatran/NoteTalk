ae98cb211a00555c2ffa8a0bcb71e4e4a6da7f3f
Git-Student1 <Maximilian.ehlert@Student.htw-berlin.de>
2022-05-12 12:52:07 +0200

    änderung in main js, nutzung von mongoose statt mongodb

--- project/main.js
+++ project/main.js
@@ -1,33 +1,16 @@
  "use strict";
- const MongoDB = require("mongodb").MongoClient,
-  dbURL = "mongodb://localhost:27017",
-  dbName = "NoteTalk_db";
- MongoDB.connect(dbURL, (error, client) => {
-  if (error) throw error;
-  let db = client.db(dbName);
-   db.collection("contacts")
-   .find()
-   .toArray((error, data) => {
-  if (error) throw error;
-  console.log(data);
-   });
- //   db.collection("contacts")
- // .insert({
- // name: "Freddie Mercury",
- // email: "fred@queen.com"
- // }, (error, db) => {
- // if (error) throw error;
- // console.log(db);
- // });
-  });
 
-
-
+ const mongoose = require("mongoose");
+ mongoose.connect(
+  "mongodb://localhost:27017/NoteTalk_db",
+  {useNewUrlParser: true}
+ );
 const express = require("express"),
 app = express();
 const homeController = require("./controllers/homeController");
 const layouts = require("express-ejs-layouts");
 const errorController = require("./controllers/errorController");
+const Substribers = require("./models/user");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -56,3 +39,30 @@ app.post("/contact", homeController.postedSignUp);
 // error handling
 app.use(errorController.respondNoResourceFound);
 app.use(errorController.respondInternalError);
+
+const db = mongoose.connection;
+
+
+db.once("open", () => {
+ console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+//const User = mongoose.model("User", signUpSchema);
+
+//create and save a user in a single step
+//TODO: dont accept signUps that dont match the schema
+Substribers.create(
+{
+  username: "Milana Tran",
+  email: "Milana.Tran@Student.htw-berlin.de",
+  password: 1234
+},
+function (error, savedDocument) {
+if (error) console.log(error);
+console.log(savedDocument);
+}
+);
+
+
+var findMilana = Substribers.findOne({username: "Milana Tran"}).where("email",/htw/);
+findMilana.exec();
---
+++ project/models/user.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose"),
+ signUpSchema = mongoose.Schema({
+ username: String,
+ email: String,
+ password: String
+});
+module.exports = mongoose.model("Substribers", signUpSchema);
