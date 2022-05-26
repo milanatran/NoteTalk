c6011bf03d2ff7b12663d479d196f35cb973f284
Git-Student1 <Maximilian.ehlert@Student.htw-berlin.de>
2022-05-26 16:59:46 +0200

    first half of chapter 17

--- project/models/user.js
+++ project/models/user.js
@@ -1,7 +1,30 @@
 const mongoose = require("mongoose"),
  signUpSchema = mongoose.Schema({
- username: String,
- email: String,
- password: String
+ username: {
+   type:String,
+   required:true,
+   unique:true,},
+
+ email: {
+   type:String,
+   required:true,
+   lowercase: true,
+   unique:true,
+ },
+ password: {
+   type:String,
+   required:true}
 });
+
 module.exports = mongoose.model("Users", signUpSchema);
+
+//TODO: think about removing the password, admins also shoouldnt know about it
+signUpSchema.methods.getInfo = function() {
+ return `Username: ${this.username} Email: ${this.email} Password: ${this.password}`;
+};
+
+signUpSchema.methods.findUser = function() {
+ return this.model("User")
+ .findOne({username: this.username})
+ .exec();
+};
