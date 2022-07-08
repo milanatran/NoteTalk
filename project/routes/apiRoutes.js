6bbcfaacfa217d82559e38ac9d54898aa34ef1ad
MILANA TRAN <MILANATRAN.NTM@gmail.com>
2022-07-08 15:57:03 +0200

    Small change made

---
+++ project/routes/apiRoutes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+//const chatroomsController = require("../controllers/chatroomsController");
+const usersController = require("../controllers/usersController");
+
+// router.use(usersController.verifyToken);
+
+router.post("/signIn", usersController.apiAuthenticate);
+router.use(usersController.verifyJWT);
+module.exports = router;
