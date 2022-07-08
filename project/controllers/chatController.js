56a3b1f0824bb9bbbdb2921d148703aac02c822e
MILANA TRAN <MILANATRAN.NTM@gmail.com>
2022-07-08 18:07:31 +0200

    Made the chatroom client server connection work

--- project/controllers/chatController.js
+++ project/controllers/chatController.js
@@ -1,8 +1,15 @@
+//Server side
 module.exports = io => {
    io.on("connection", client => {
-     console.log("new connection");
+      // console.log("new connection");
+     client.on('send-chat-message', message => {
+       //Send the message to every client connected
+       client.broadcast.emit('chat-message', message);
+     });
+
+
      client.on("disconnect", () => {
-       console.log("user disconnected");
+       // console.log("user disconnected");
      });
      client.on("message", () => {
       io.emit("message", {
--- project/public/js/noteTalkApp.js
+++ project/public/js/noteTalkApp.js
@@ -1,14 +1,40 @@
-const socket = io();
-$("#chatForm").submit(() => {
- socket.emit("message");
- $("#chat-input").val("");
- return false;
+//Client Side
+const socket = io('http://localhost:3000');
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+
+socket.on('chat-message', data => {
+  appendMessage(data);
 });
 
-socket.on("message", (message) => {
- displayMessage(message.content);
+messageForm.addEventListener('submit', e => {
+  //Stop site from refreshing so the chat does not disappear
+  e.preventDefault();
+  const message = messageInput.value;
+  socket.emit('send-chat-message', message);
+  messageInput.value = '';
 });
 
-let displayMessage = (message) => {
- $("#chat").prepend($("<li>").html(message));
-};
+// add the messages to the page
+function appendMessage(message){
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+}
+// $(document).ready(() => {
+//   const socket = io();
+//   $("#chatForm").submit(() => {
+//    socket.emit("message");
+//    $("#chat-input").val("");
+//    return false;
+//   });
+//
+//   socket.on("message", (message) => {
+//    displayMessage(message.content);
+//   });
+//
+//   let displayMessage = (message) => {
+//    $("#chat").prepend($("<li>").html(message));
+//   };
+// }
